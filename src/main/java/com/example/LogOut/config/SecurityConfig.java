f243fe69a20513d0ebb7cd0bb932ec3fef04d7c9
Elanchiyam <elanchiyam1322000@gmail.com>
2023-02-06 16:40:46 +0530

    Simple OAuth Application

---
+++ src/main/java/com/example/LogOut/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.LogOut.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .requestMatchers("/**", "/error", "/webjars/**","logout").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf().disable()
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .oauth2Login();
+    return http.build();
+    }
+
+}
---
+++ src/test/java/com/example/SimpleOAuth/SimpleOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
